3c100255d9f35e004b48b8fad807cebb834a3f8c
Rickle, Griffin (ARC-TH)[ASRC Federal Data Solutions, LLC] <griffin.e.rickle@nasa.gov>
2025-11-21 13:44:52 -0500

    Cleaned lua/bash-config of NASA specific references

--- lua/bash-config.lua
+++ lua/bash-config.lua
@@ -1,9 +1,20 @@
+-- lua/bash-config.lua
+local ok, local_config = pcall(require, 'local')
+if not ok then
+    vim.notify("local.lua not found. Bash LSP will use system default.", vim.log.levels.WARN)
+    local_config = { nvm_node_path = "" }
+end
+
+local bash_lsp_cmd = local_config.nvm_node_path ~= "" 
+    and local_config.nvm_node_path .. '/bin/bash-language-server'
+    or 'bash-language-server'
+
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'sh',
   callback = function()
     vim.lsp.start({
       name = 'bash-language-server',
-      cmd = { '/home/grickle/.nvm/versions/node/v18.19.1/bin/bash-language-server', 'start' },
+      cmd = { bash_lsp_cmd, 'start' },
     })
   end,
 })
