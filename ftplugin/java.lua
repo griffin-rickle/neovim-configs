8f6ba3d69fd37a2d24a2ecb4f75b9525deef5be2
Rickle, Griffin (ARC-TH)[ASRC Federal Data Solutions, LLC] <griffin.e.rickle@nasa.gov>
2025-11-21 13:42:06 -0500

    Clean ftplugin/java.lua of NASA specific configurations

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,7 +1,13 @@
+local ok, local_config = pcall(require, 'local')
+if not ok then
+    vim.notify("local.lua not found. Copy local.lua.example to local.lua and customize it.", vim.log.levels.ERROR)
+    return
+end
+
 -- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
-local workspace_dir = '/home/grickle/git/workspaces/' .. project_name
+local workspace_dir = local_config.workspace_dir .. project_name
 local home = os.getenv("HOME")
 -- local jdtls_path = home .. "/.local/share/nvim/mason/packages/jdtls"
 local jdtls_path = home .. "/bin/jdt/"
@@ -18,7 +24,7 @@ end
 local launcher_jar = find_launcher_jar()
 
 local java_debug_bundle = vim.fn.glob(
-  "/home/grickle/git/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+  local_config.java_debug_path .. "/com.microsoft.java.debug.plugin-*.jar"
 )
 
 local bundles = {}
@@ -28,11 +34,8 @@ if java_debug_bundle ~= "" then
 end
 
 local config = {
-    -- cmd = { "/home/grickle/bin/jdt/bin/jdtls",
-    -- cmd = { home .. "/.local/share/nvim/mason/packages/jdtls/bin/jdtls" },
     cmd = {
         '/usr/lib/jvm/java-21-openjdk-21.0.8.0.9-1.el8.x86_64/bin/java',
-    --     '/usr/lib/jvm/java-17-openjdk-17.0.14.0.7-3.el8.x86_64/bin/java',
         '-Declipse.application=org.eclipse.jdt.ls.core.id1',
         '-Dosgi.bundles.defaultStartLevel=4',
         '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -44,26 +47,11 @@ local config = {
         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
         '-jar', launcher_jar,
         "-configuration", home .. "/bin/jdt/config_linux",
-        -- "-configuration", home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
-        -- '-jar', '/home/grickle/git/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/products/languageServer.product/linux/gtk/x86_64/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
-    --     '-configuration', '/home/grickle/git/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux',
         '-data', workspace_dir
     },
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
     init_options = {
         bundles = bundles
-    },
-    -- settings = {
-    --     java = {
-    --         configuration = {
-    --             runtimes = {
-    --                 {
-    --                     name = "JavaSE-17",
-    --                     path = "/usr/lib/jvm/java-17-openjdk-17.0.15.0.6-2.el8.x86_64/"
-    --                 }
-    --             }
-    --         }
-    --     }
-    -- }
+    }
 }
 require('jdtls').start_or_attach(config)
