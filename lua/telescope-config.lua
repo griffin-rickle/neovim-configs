3b9ab25dbf241949a396a1023dfb55edb559e739
Rickle, Griffin (ARC-TH)[ASRC Federal Data Solutions, LLC] <griffin.e.rickle@nasa.gov>
2026-01-07 16:09:15 -0500

    Telescope ignores unneeded file globs

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,5 +1,24 @@
 require("telescope").setup {
     defaults = {
+        file_ignore_patterns = {
+            "node_modules/.*",
+            "venv/.*",
+            ".venv/.*",
+            "env/.*",
+            ".env/.*",
+            ".git/.*",
+            "%.o",
+            "%.a",
+            "%.out",
+            "%.class",
+            "%.pdf",
+            "%.mkv",
+            "%.mp4",
+            "%.zip",
+            "__pycache__/.*",
+            "%.pyc",
+            "%.pyi",
+        },
         preview = {
             filesize_limit = 1
         }
